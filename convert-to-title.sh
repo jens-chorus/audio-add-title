e1cd32f78831094e4dd7ba59720135edb5fc2185
Jens Chorus <jens.chorus@raisenow.com>
2023-08-04 09:44:45 +0200

    Indent case statements

--- convert-to-title.sh
+++ convert-to-title.sh
@@ -2,10 +2,10 @@
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-  --locale)
-    locale="$2"
-    shift
-    ;;
+    --locale)
+      locale="$2"
+      shift
+      ;;
   esac
   shift
 done
@@ -22,22 +22,22 @@ print_error() {
 detect_environment() {
   raw_env="$(uname -sr)"
   case ${raw_env} in
-  Darwin*)
-    echo "${ENV_OSX}"
-    ;;
-  # Linux*Microsoft*)
-  #   env='WSL' # Windows Subsystem for Linux
-  #   ;;
-  Linux*)
-    echo "${ENV_LINUX}"
-    ;;
-  # CYGWIN* | MINGW* | MINGW32* | MSYS*)
-  #   env="MSW"
-  #   ;;
-  *)
-    print_error "Detected unsupported environment: ${raw_env}"
-    exit 0
-    ;;
+    Darwin*)
+      echo "${ENV_OSX}"
+      ;;
+    # Linux*Microsoft*)
+    #   env='WSL' # Windows Subsystem for Linux
+    #   ;;
+    Linux*)
+      echo "${ENV_LINUX}"
+      ;;
+    # CYGWIN* | MINGW* | MINGW32* | MSYS*)
+    #   env="MSW"
+    #   ;;
+    *)
+      print_error "Detected unsupported environment: ${raw_env}"
+      exit 0
+      ;;
   esac
 }
 
@@ -47,22 +47,22 @@ if [ -z ${locale+x} ]; then
 fi
 
 case $env in
-${ENV_OSX})
-  speaker=$(say -v \? | grep "$locale" | awk 'NR==1{print $1}')
-  echo "speaker " $speaker
-  for inputfile in *; do
-    echo "$inputfile"
-    title=$(exiftool -json "$inputfile" | jq -r '.[0].Title') && echo $title && say -v $speaker "$title" -o title.wav --data-format=LEF32@22050
-    ffmpeg -i title.wav -i "$inputfile" -filter_complex "[0:a:0][1:a:0]concat=n=2:v=0:a=1[outa]" -map "[outa]" -map_metadata 1 -ab 128k -ac 2 -ar 44100 "./with-intro/${inputfile%.*}-$title.with_intro.${inputfile##*.}"
-  done
-  ;;
-${ENV_LINUX})
-  for inputfile in *; do
-    echo "$inputfile"
-    exiftool -json "$inputfile" | jq '.[0].Title' | espeak -vde -w /tmp/title.wav
-    ffmpeg -i /tmp/title.wav -i "$inputfile" -filter_complex "[0:a:0][1:a:0]concat=n=2:v=0:a=1[outa]" -map "[outa]" -map_metadata 1 -ab 128k -ac 2 -ar 44100 "${inputfile%.*}.with_intro.${inputfile##*.}"
-  done
-  ;;
+  ${ENV_OSX})
+    speaker=$(say -v \? | grep "$locale" | awk 'NR==1{print $1}')
+    echo "speaker " $speaker
+    for inputfile in *; do
+      echo "$inputfile"
+      title=$(exiftool -json "$inputfile" | jq -r '.[0].Title') && echo $title && say -v $speaker "$title" -o title.wav --data-format=LEF32@22050
+      ffmpeg -i title.wav -i "$inputfile" -filter_complex "[0:a:0][1:a:0]concat=n=2:v=0:a=1[outa]" -map "[outa]" -map_metadata 1 -ab 128k -ac 2 -ar 44100 "./with-intro/${inputfile%.*}-$title.with_intro.${inputfile##*.}"
+    done
+    ;;
+  ${ENV_LINUX})
+    for inputfile in *; do
+      echo "$inputfile"
+      exiftool -json "$inputfile" | jq '.[0].Title' | espeak -vde -w /tmp/title.wav
+      ffmpeg -i /tmp/title.wav -i "$inputfile" -filter_complex "[0:a:0][1:a:0]concat=n=2:v=0:a=1[outa]" -map "[outa]" -map_metadata 1 -ab 128k -ac 2 -ar 44100 "${inputfile%.*}.with_intro.${inputfile##*.}"
+    done
+    ;;
 esac
 
 exit 1
