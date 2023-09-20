d17fb6aa93061034577c2970ea4145237fc3d37d
Kalle Kruse <kalle.kruse@rocketmail.com>
2023-09-20 02:36:50 +0200

    feat: improve linux speech and deduplicate code

--- convert-to-title.sh
+++ convert-to-title.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 
 # Define supported audio formats
-audio_formats=("mp3" "mp4" "wav" "flac" "aac" "ogg")
+audio_formats=("mp3" "mp4" "wav" "flac" "aac" "ogg" "m4a")
 
 readonly ENV_OSX='Mac OS X'
 readonly ENV_LINUX='Linux'
@@ -30,8 +30,25 @@ get_exif_metadata() {
   filename_without_ext="${file_name%.*}"
   sanitized_filename_without_ext=$(echo "$filename_without_ext" | iconv -f utf8 -t ascii//TRANSLIT | sed 's/[^a-zA-Z0-9]//g')
   sanitized_filename_with_ext="${sanitized_filename_without_ext}.${file_type_extension}"
+  
   file_sample_rate=$(echo "$metadata" | jq -r '.[0].SampleRate')
-  file_kbit_rate=$(echo "$metadata" | jq -r '.[0].AudioBitrate' | awk 'NR==1{print $1}')
+  if [[ "${file_sample_rate}" == "null" ]]; then
+    file_sample_rate=$(echo "$metadata" | jq -r '.[0].AudioSampleRate')
+  fi
+  if [[ "${file_sample_rate}" == "null" ]]; then
+    print_error "Could not determine sample rate for ${inputfile}"
+    exit 1
+  fi
+  
+  file_bitrate=$(echo "$metadata" | jq -r '.[0].AudioBitrate')
+  if [[ "${file_bitrate}" == "null" ]]; then
+    file_bitrate=$(echo "$metadata" | jq -r '.[0].AvgBitrate')
+  fi
+  if [[ "${file_bitrate}" == "null" ]]; then
+    print_error "Could not determine bit rate for ${inputfile}"
+    exit 1
+  fi
+  file_kbit_rate=$(echo "$file_bitrate" | awk 'NR==1{print $1}')
   file_bit_rate=$(($file_kbit_rate * 1000))
 }
 
@@ -45,39 +62,41 @@ while [[ "$#" -gt 0 ]]; do
   esac
 done
 
-mkdir -p with-intro
+for inputfile in *; do
+  get_exif_metadata
 
-case $env in
-  ${ENV_OSX})
-    speaker=$(say -v \? | grep "$locale" | awk 'NR==1{print $1}')
+  if [[ ! " ${audio_formats[@]} " =~ " ${file_type_extension} " ]]; then
+    echo "Skipping file: $inputfile" 
+    continue
+  fi
 
-    for inputfile in *; do
-      get_exif_metadata
+  echo "Processing file: $inputfile"
 
-      if [[ ! " ${audio_formats[@]} " =~ " ${file_type_extension} " ]]; then
-        continue
-      fi
+  case $env in
+    ${ENV_OSX})
+      title_wav="title.wav"
+      speaker=$(say -v \? | grep "$locale" | awk 'NR==1{print $1}')
+      say -v $speaker "$title" -o "${title_wav}" --data-format=LEF32@22050
+      mkdir -p with-intro
+      output_file="./with-intro/${file_name}"
+      ;;
+    ${ENV_LINUX})
+      title_wav="/tmp/title.wav"
+      tts --model_name tts_models/de/thorsten/tacotron2-DDC --out_path "${title_wav}" --text "$title"
+      output_file="${inputfile%.*}.with_intro.${inputfile##*.}"
+      ;;
+  esac
+  
+  title_converted=title."$file_type_extension"
+  ffmpeg -i "${title_wav}" -vn -ar "$file_sample_rate" -ac 2 -b:a "$file_bit_rate" "${title_converted}"
+  cp "$inputfile" "$sanitized_filename_with_ext"
+  # Using the ffmpeg's Concat protocol as follows would be more elegant but does not work e.g. for formats with metadata elements, like .m4a:
+  #   ffmpeg -i "concat:${title_converted}|${sanitized_filename_with_ext}" -acodec copy "${output_file}"
+  ffmpeg -f concat -safe 0 -i <(echo "file '$PWD/${title_converted}'"; echo "file '$PWD/${sanitized_filename_with_ext}'") -acodec copy "${output_file}"
+  rm "${title_wav}" "${title_converted}" "$sanitized_filename_with_ext"
 
-      echo "Processing file: $inputfile"
+  echo "Finished processing $inputfile"
 
-      say -v $speaker "$title" -o title.wav --data-format=LEF32@22050
-      ffmpeg -i title.wav -vn -ar "$file_sample_rate" -ac 2 -b:a "$file_bit_rate" title."$file_type_extension"
-      cp "$inputfile" "$sanitized_filename_with_ext"
-      ffmpeg -i "concat:title.${file_type_extension}|${sanitized_filename_with_ext}" -acodec copy "./with-intro/${file_name}"
-
-      rm title.wav title."$file_type_extension" "$sanitized_filename_with_ext"
-
-      echo "Finished processing $file_name"
-    done
-    ;;
-
-  ${ENV_LINUX})
-    for inputfile in *; do
-      echo "$inputfile"
-      exiftool -json "$inputfile" | jq '.[0].Title' | espeak -vde -w /tmp/title.wav
-      ffmpeg -i /tmp/title.wav -i "$inputfile" -filter_complex "[0:a:0][1:a:0]concat=n=2:v=0:a=1[outa]" -map "[outa]" -map_metadata 1 -ab 128k -ac 2 -ar 44100 "${inputfile%.*}.with_intro.${inputfile##*.}"
-    done
-    ;;
-esac
+done
 
 exit 0
